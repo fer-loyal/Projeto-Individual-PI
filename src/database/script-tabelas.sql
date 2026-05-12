5307ec920bef24adf8e34ed4992d107656ac7eb6
fer-loyal <fernanda.leal@sptech.school>
2026-05-12 10:32:41 -0300

    Adicionando o restante do HTML e CSS juntamente ao quiz

---
+++ src/database/script-tabelas.sql
@@ -0,0 +1,60 @@
+-- Arquivo de apoio, caso você queira criar tabelas como as aqui criadas para a API funcionar.
+-- Você precisa executar os comandos no banco de dados para criar as tabelas,
+-- ter este arquivo aqui não significa que a tabela em seu BD estará como abaixo!
+
+/*
+comandos para mysql server
+*/
+
+CREATE DATABASE SpiralOutnet ;
+USE SpiralOutnet;
+
+CREATE TABLE usuario (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	nome VARCHAR(50),
+	email VARCHAR(50),
+	senha VARhCHAR(50)
+);
+
+CREATE TABLE quiz (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	certas INT,
+	erradas INT,
+	porcentagem INT,
+	data_horario DATETIME DEFAULT NOW(),
+	id_usuario INT,
+	CONSTRAINT fk_usuario
+	FOREIGN KEY (id_usuario) REFERENCES usuario(id)
+);
+
+CREATE TABLE resultado (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	porcentagem INT,
+
+);
+
+
+CREATE TABLE (
+
+	
+);
+CREATE TABLE queatoes()
+
+/* esta tabela deve estar de acordo com o que está em INSERT de sua API do arduino - dat-acqu-ino */
+
+create table medida (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	dht11_umidade DECIMAL,
+	dht11_temperatura DECIMAL,
+	luminosidade DECIMAL,
+	lm35_temperatura DECIMAL,
+	chave TINYINT,
+	momento DATETIME,
+	fk_aquario INT,
+	FOREIGN KEY (fk_aquario) REFERENCES aquario(id)
+);
+
+insert into empresa (razao_social, codigo_ativacao) values ('Empresa 1', 'ED145B');
+insert into empresa (razao_social, codigo_ativacao) values ('Empresa 2', 'A1B2C3');
+insert into aquario (descricao, fk_empresa) values ('Aquário de Estrela-do-mar', 1);
+insert into aquario (descricao, fk_empresa) values ('Aquário de Peixe-dourado', 2);
