e6a7dd72cbeb3b5f73af2398951f93eb3e182b36
fer-loyal <fernanda.leal@sptech.school>
2026-05-23 00:32:05 -0300

    criando o controller do quiz

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ var medidasRouter = require("./src/routes/medidas");
 var aquariosRouter = require("./src/routes/aquarios");
 var empresasRouter = require("./src/routes/empresas");
 var dashboardRouter = require("./src/routes/dashboard");
+var quizRouter = require("./routes/quizRouter");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -36,6 +37,8 @@ app.use("/medidas", medidasRouter);
 app.use("/aquarios", aquariosRouter);
 app.use("/empresas", empresasRouter);
 app.use("/dashboard", dashboardRouter);
+app.use("/quiz", quizRouter);
+
 
 app.listen(PORTA_APP, function () {
     console.log(`
---
+++ src/routes/quizRouter.js
@@ -0,0 +1,10 @@
+var express =  require("express");
+var router = express.Router();
+
+var quizController = require("../controllers/quizController")
+/* recebe os dados do quiz */
+router.post("/salvar", function (req, res){
+    quizController.salvarResultado(req, res);
+});
+
+module.exports = router;
